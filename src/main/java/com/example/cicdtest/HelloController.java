10b86c1bb209bb4cf4fdb3a5913068fe83056b05
Yejin-source <yejin3831@naver.com>
2025-06-20 14:02:46 +0900

    commit 3

--- src/main/java/com/example/cicdtest/HelloController.java
+++ src/main/java/com/example/cicdtest/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping("/")
 	public String hello() {
-		return "#2 hello";
+		return "#3 hello";
 	}
 	
 }
